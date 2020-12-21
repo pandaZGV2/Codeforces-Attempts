2aecde1c540aeba3a883266c049e5180f9c76917
PandaZGV2 <aaronmonis22@gmail.com>
2020-12-21 12:50:40 +0400

    Added C_Busy_Robot

--- C_Busy_Robot.cpp
+++ C_Busy_Robot.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -10,44 +10,49 @@ typedef pair<int, int> pi;
 #define s second
 #define pb push_back
 #define mp make_pair
-#define loop(n) for(int i=0; i<n; i++)
-#define rep(i, a, n) for(int i=a; i<n; i++)
-#define file_read freopen("input.txt", "r", stdin); \
-                  freopen("output.txt", "w", stdout);
-#define tc int t; cin>>t; while(t--)
+#define loop(n) for (int i = 0; i < n; i++)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
 #define endl "\n"
-#define iamspeed cin.tie(0) -> sync_with_stdio(0)
+#define iamspeed cin.tie(0)->sync_with_stdio(0)
 
-
-
-
-int main(void){
+int main(void)
+{
     //iamspeed;
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         int n;
-        cin>>n;
-        vector< pair<int,int>> p;
-        int t,x,x1=0,t1=0;
-        for(int i=0;i<n;i++)
+        cin >> n;
+        vector<pair<int, int>> p;
+        int s = 0;
+        int t, x, x1 = 0, t1 = 0;
+        for (int i = 0; i < n; i++)
         {
-            cin>>t>>x;
-            if(t1<=t || i==0)
+            cin >> t >> x;
+            if (t1 <= t || i == 0)
             {
-                t1+=t-t1+abs(x-x1);
-                x1+=x;
-                p.push_back(make_pair(t,x));
+                // if (t == t1)
+                // {
+                //     s++;
+                // }
+                t1 += t - t1 + abs(x - x1);
+                x1 += x;
+
+                p.push_back(make_pair(t, x));
             }
             else
             {
                 continue;
             }
-                     
         }
-        cout<<p.size()<<endl;
+        cout << s << endl;
     }
-    
-
 }
