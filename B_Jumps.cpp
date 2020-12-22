b3f1420719773df8144f6b7563ebe17c89b65baa
PandaZGV2 <aaronmonis22@gmail.com>
2020-12-22 12:09:59 +0400

    B_jumps

---
+++ B_Jumps.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long int ull;
+typedef long int li;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define modint int(1e9 + 7)
+#define mod 1e9 + 7
+#define loop(n) for (int i = 0; i < n; i++)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl "\n"
+#define iamspeed cin.tie(0)->sync_with_stdio(0)
+int main(void)
+{
+    iamspeed;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y = 0, k = 0;
+        cin >> x;
+
+        while (k * (k + 1) < 2 * x)
+        {
+            k++;
+        }
+        while(k*(k+1)/2==x+1)
+        {
+            k++;
+            x--;
+        }
+        cout << k << endl;
+    }
+
+    return 0;
+}
