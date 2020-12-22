9a0f69115b16494d41a3b35e205f850eea8de8db
PandaZGV2 <aaronmonis22@gmail.com>
2020-12-22 12:11:00 +0400

    D_Add_to_neighbour_and_remove

--- D_Add_to_Neighbour_and_Remove.cpp
+++ D_Add_to_Neighbour_and_Remove.cpp
@@ -1,22 +1,82 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int main()
+
+typedef long long ll;
+typedef unsigned long long int ull;
+typedef long int li;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define modint int(1e9 + 7)
+#define mod 1e9 + 7
+#define loop(n) for (int i = 0; i < n; i++)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl "\n"
+#define iamspeed cin.tie(0)->sync_with_stdio(0)
+void solve(int n)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    int a[n];
+    int s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        s += a[i];
+    }
+    int cursum = 0;
+    bool f;
+    int i;
+    for (i = n; i >= 1; i--)
+    {
+        cursum = 0;
+        int each = s / i;
+        if (s % i == 0)
+        {
+            f = true;
+            for (int j = 0; j < n; j++)
+            {
+                cursum += a[j];
+                if (cursum > each)
+                {
+                    f = false;
+                    break;
+                }
+                else if (cursum == each)
+                {
+                    f = true;
+                    cursum = 0;
+                }
+            }
+            if (f == true)
+            {
+                cout << n - i << endl;
+                return;
+            }
+        }
+    }
+}
+int main(void)
+{
+    iamspeed;
     int t;
+    int n;
     cin >> t;
     while (t--)
     {
-        int n;
         cin >> n;
-        int c;
-        vector<int> a;
-        for (int i = 0; i < n; i++)
-        {
-            cin >> c;
-            a.push_back(c);
-        }
+        solve(n);
     }
+
     return 0;
 }
