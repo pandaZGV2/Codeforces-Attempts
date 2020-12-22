240d157c35cf10cb9306600a78d5a0447434fe7f
PandaZGV2 <aaronmonis22@gmail.com>
2020-12-22 12:08:17 +0400

    A_Red_Blue

--- A_Red_Blue_Shuffle.cpp
+++ A_Red_Blue_Shuffle.cpp
@@ -12,8 +12,8 @@ typedef pair<int, int> pi;
 #define s second
 #define pb push_back
 #define mp make_pair
-#define modint int(1e9+7)
-#define mod 1e9+7
+#define modint int(1e9 + 7)
+#define mod 1e9 + 7
 #define loop(n) for (int i = 0; i < n; i++)
 #define rep(i, a, n) for (int i = a; i < n; i++)
 #define file_read                     \
@@ -28,44 +28,37 @@ typedef pair<int, int> pi;
 
 int main(void)
 {
-    iamspeed;
+    //iamspeed;
     int t;
     cin >> t;
     while (t--)
     {
         int n;
         cin >> n;
-        int c;
-        int s1=0,s2=0;
-        // for (int i = 0; i < n; i++)
-        // {
-            cin>>c;
-            while(c>0)
-            {
-                s1+=c%10;
-                c=c/10;
-            }
-            
-        // }
-        //cout<<s1<<" ";
-        // for (int i = 0; i < n; i++)
-        // {
-            cin>>c;
-            while(c>0)
-            {
-                s2+=c%10;
-                c=c/10;
-            }
-            
-        // }
-        //cout<<s2<<endl;
-        if (s2 > s1)
+        char c[10000], c1[10000];
+        int s1 = 0, s2 = 0;
+        int a, b;
+        cin >> c >> c1;
+        for(int i=0;i<n;i++)
         {
-            cout << "BLUE" << endl;
+            if(c[i]==c1[i])
+            ;
+            else if(c[i]>c1[i])
+            s1++;
+            else
+            {
+                s2++;
+            }
+            
         }
-        else if (s1 > s2)
+        
+        if (s1 > s2)
         {
             cout << "RED" << endl;
+        }
+        else if (s1 < s2)
+        {
+            cout << "BLUE" << endl;
         }
         else
         {
