827c2a598093975885cc19fc231f41af96963144
PandaZGV2 <aaronmonis22@gmail.com>
2020-12-23 10:20:49 +0400

    Added Distance.py

---
+++ 006_Distance.py
@@ -0,0 +1,9 @@
+import math
+def solve():
+    x1=float(input(""))
+    y1=float(input(""))
+    x2=float(input(""))
+    y2=float(input(""))
+    print(math.sqrt(pow(x1-x2,2)+pow(y1-y2,2)))
+solve()
+
