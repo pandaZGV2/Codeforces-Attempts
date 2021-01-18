77edd79da5b9b40d5f121278319526bd5e08db87
PandaZGV2 <aaronmonis22@gmail.com>
2021-01-18 17:13:35 +0400

    C_Bouncing_Ball

---
+++ C_Bouncing_Ball.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long int ull;
+typedef long int li;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define modint int(1e9 + 7)
+#define mod 1e9 + 7
+#define Inf INFINITY
+
+// =============================Graph Agorithms==================================
+// vector<bool> visited;
+
+// void dfs(int v)
+// {
+//     visited[v] = true;
+//     for (int u : adj[v])
+//     {
+//         if (!visited[u])
+//             dfs(u);
+//     }
+// }
+// vector<vector<int>> adj; // adjacency list representation
+// int n;                   // number of nodes
+// int s;                   // source vertex
+
+// queue<int> q;
+// vector<bool> used(n);
+// vector<int> d(n), p(n);
+
+// q.push(s);
+// used[s] = true;
+// p[s] = -1;
+// while (!q.empty())
+// {
+//     int v = q.front();
+//     q.pop();
+//     for (int u : adj[v])
+//     {
+//         if (!used[u])
+//         {
+//             used[u] = true;
+//             q.push(u);
+//             d[u] = d[v] + 1;
+//             p[u] = v;
+//         }
+//     }
+// }
+
+#define loop(n) for (int i = 0; i < n; i++)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl "\n"
+#define iamspeed                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+int main(void)
+{
+    iamspeed;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,p,k;
+        cin>>n>>p>>k;
+        vi a(n+k+1,0);
+        char c;
+        for(int i=1;i<=n;i++)
+        {
+            cin>>c;
+            a[i]=!(c-'0');
+        }
+        int x,y;
+        cin>>x>>y;
+        for(int i=n;i>=1;i--)
+        {
+            if(i+k<=n)
+            {
+                a[i]+=a[i+k];
+            }
+        }
+        int ans=(int)Inf;
+        for(int i=p;i<=n;i++)
+        {
+            ans=min(ans, (i-p)*y+a[i]*x);
+        }
+        cout<<ans<<endl;
+
+    }
+
+    return 0;
+}
