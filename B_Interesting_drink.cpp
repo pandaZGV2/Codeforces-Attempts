feb6076c1f792e02e776a698a0761610416cdf7e
PandaZGV2 <aaronmonis22@gmail.com>
2021-01-18 17:13:02 +0400

    Interesting Drink

---
+++ B_Interesting_drink.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long int ull;
+typedef long int li;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define modint int(1e9 + 7)
+#define mod 1e9 + 7
+#define Inf INFINITY
+
+// =============================Graph Agorithms==================================
+// vector<bool> visited;
+
+// void dfs(int v)
+// {
+//     visited[v] = true;
+//     for (int u : adj[v])
+//     {
+//         if (!visited[u])
+//             dfs(u);
+//     }
+// }
+// vector<vector<int>> adj; // adjacency list representation
+// int n;                   // number of nodes
+// int s;                   // source vertex
+
+// queue<int> q;
+// vector<bool> used(n);
+// vector<int> d(n), p(n);
+
+// q.push(s);
+// used[s] = true;
+// p[s] = -1;
+// while (!q.empty())
+// {
+//     int v = q.front();
+//     q.pop();
+//     for (int u : adj[v])
+//     {
+//         if (!used[u])
+//         {
+//             used[u] = true;
+//             q.push(u);
+//             d[u] = d[v] + 1;
+//             p[u] = v;
+//         }
+//     }
+// }
+
+#define loop(n) for (int i = 0; i < n; i++)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl "\n"
+#define iamspeed                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+int binarysearch(vi &a, int n, int b)
+{
+    int mid = 0;
+    int l = 0, r = n - 1;
+    int ans = -1;
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+        if (a[mid] <= b)
+        {
+            ans = mid;
+            l = mid + 1;
+        }
+        else if (a[mid] > b)
+        {
+            r = mid - 1;
+        }
+    }
+    return ans;
+}
+int main(void)
+{
+    iamspeed;
+    int n;
+    cin >> n;
+    vi a;
+    int c;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c;
+        a.pb(c);
+    }
+    int q;
+    cin >> q;
+    vi b(q);
+    for (int i = 0; i < q; i++)
+    {
+        cin >> b[i];
+    }
+    sort(a.begin(),a.end());
+    // sort(b.rbegin(),b.rend());
+    int ans = 0;
+    for (int i = 0; i < q; i++)
+    {
+        ans = binarysearch(a, n, b[i]);
+        cout
+            << ans+1 << endl;
+    }
+
+    return 0;
+}
