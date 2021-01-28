d7f67e1c01c143db99eb7652469ada26edbac9a5
PandaZGV2 <aaronmonis22@gmail.com>
2021-01-28 23:15:15 +0400

    C_Alphabet Removal

---
+++ C_Alphabetic_Removals.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long int ull;
+typedef long int li;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define modint int(1e9 + 7)
+#define mod 1e9 + 7
+#define Inf INFINITY
+
+// =============================Graph Agorithms==================================
+// vector<bool> visited;
+
+// void dfs(int v)
+// {
+//     visited[v] = true;
+//     for (int u : adj[v])
+//     {
+//         if (!visited[u])
+//             dfs(u);
+//     }
+// }
+// vector<vector<int>> adj; // adjacency list representation
+// int n;                   // number of nodes
+// int s;                   // source vertex
+
+// queue<int> q;
+// vector<bool> used(n);
+// vector<int> d(n), p(n);
+
+// q.push(s);
+// used[s] = true;
+// p[s] = -1;
+// while (!q.empty())
+// {
+//     int v = q.front();
+//     q.pop();
+//     for (int u : adj[v])
+//     {
+//         if (!used[u])
+//         {
+//             used[u] = true;
+//             q.push(u);
+//             d[u] = d[v] + 1;
+//             p[u] = v;
+//         }
+//     }
+// }
+
+#define loop(n) for (int i = 0; i < n; i++)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl "\n"
+#define iamspeed                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+int main(void)
+{
+    iamspeed;
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    vector<vi> a(26, vi(n, 0));
+    for (int i = 0; i < n; i++)
+    {
+        a[s[i] - 'a'][i]++;
+    }
+    for (int i = 0; i < 26 && k; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i][j] >= 1)
+            {
+                k--;
+                s[j] = '0';
+            }
+            if (k == 0)
+            {
+                goto end;
+            }
+        }
+    }
+
+end:;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] != '0')
+            cout << s[i];
+    }
+
+    return 0;
+}
