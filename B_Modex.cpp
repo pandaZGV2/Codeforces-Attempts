84af10f050d59262f6069e17fc7507030192ea37
PandaZGV2 <aaronmonis22@gmail.com>
2021-02-06 18:23:59 +0400

    B_Modex

--- B_Modex.cpp
+++ B_Modex.cpp
@@ -12,8 +12,8 @@ typedef pair<int, int> pi;
 #define s second
 #define pb push_back
 #define mp make_pair
-#define modint int(1e9 + 7)
-#define mod 1e9 + 7
+// #define modint int(1e9 + 7)
+// #define mod 1e9 + 7
 #define loop(n) for (int i = 0; i < n; i++)
 #define rep(i, a, n) for (int i = a; i < n; i++)
 #define file_read                     \
@@ -30,10 +30,25 @@ long long binpow(long long a, long long b, long long m)
     if (b == 0)
         return 1;
     long long res = binpow(a, b / 2, m);
-    if (b % 2)
+    if (b % 2) //b&1==0
         return (res % m * res % m * a % m) % m;
-    else
+    else //b&1==1
         return (res % m * res % m) % m;
+}
+ll binwhile(ll a, ll b, ll modint)
+{
+    a%=modint;
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1 == 1)
+        {
+            res = (res * a)%modint;
+        }
+        a = (a * a)%modint;
+        b >>= 1;
+    }
+    return res;
 }
 int main(void)
 {
@@ -43,8 +58,9 @@ int main(void)
     {
         ll x, y, m;
         cin >> x >> y >> m;
-        cout << binpow(x, y, m)<<endl;
+        cout << binwhile(x, y, m) << endl;
     }
     cin >> z;
+    // cout<<binwhile(3,5)<<endl;
     return 0;
 }
