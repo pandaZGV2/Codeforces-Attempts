de72ab0391c84944d242ef34416753897ad87159
PandaZGV2 <aaronmonis22@gmail.com>
2021-03-06 12:51:26 +0400

    A_FLOOR_NUMBER

---
+++ A_Floor_Number.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, x;
+        cin>>n>>x;
+        if(n<=2)
+        {
+            cout<<1<<"\n";
+        }
+        else
+        {
+            cout<<(n-3)/x+2<<"\n";
+        }
+    }
+    return 0;
+}
