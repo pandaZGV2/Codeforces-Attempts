cc9e1426c39ca1410ccac7f7ee847b61b5c33ea5
PandaZGV2 <aaronmonis22@gmail.com>
2021-03-06 12:50:36 +0400

    A_BORING APARTMENT

---
+++ A_Boring_Apartments.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long int ull;
+typedef long int li;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define modint int(1e9 + 7)
+#define mod 1e9 + 7
+#define Inf INFINITY
+
+// =============================Graph Agorithms==================================
+// vector<bool> visited;
+
+// void dfs(int v)
+// {
+//     visited[v] = true;
+//     for (int u : adj[v])
+//     {
+//         if (!visited[u])
+//             dfs(u);
+//     }
+// }
+// vector<vector<int>> adj; // adjacency list representation
+// int n;                   // number of nodes
+// int s;                   // source vertex
+
+// queue<int> q;
+// vector<bool> used(n);
+// vector<int> d(n), p(n);
+
+// q.push(s);
+// used[s] = true;
+// p[s] = -1;
+// while (!q.empty())
+// {
+//     int v = q.front();
+//     q.pop();
+//     for (int u : adj[v])
+//     {
+//         if (!used[u])
+//         {
+//             used[u] = true;
+//             q.push(u);
+//             d[u] = d[v] + 1;
+//             p[u] = v;
+//         }
+//     }
+// }
+
+#define loop(n) for (int i = 0; i < n; i++)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl "\n"
+#define iamspeed                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+int main(void)
+{
+    iamspeed;
+    tc
+    {
+        int number[4] = {1, 11, 111, 1111};
+        int cnt = 0;
+        int x;
+        cin >> x;
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                cnt += j + 1;
+                if (number[j] * i == x)
+                {
+                    goto L;
+                }
+            }
+        }
+    L:;
+        cout << cnt << endl;
+    }
+
+    return 0;
+}
