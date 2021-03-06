c799ca7a6276853c52f42d7e324692eea84cdac4
PandaZGV2 <aaronmonis22@gmail.com>
2021-03-06 12:56:42 +0400

    KAPREKAR

---
+++ C_Kaprekar_Number.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long int ull;
+typedef long int li;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define modint int(1e9 + 7)
+#define mod 1e9 + 7
+#define Inf INFINITY
+
+// =============================Graph Agorithms==================================
+// vector<bool> visited;
+
+// void dfs(int v)
+// {
+//     visited[v] = true;
+//     for (int u : adj[v])
+//     {
+//         if (!visited[u])
+//             dfs(u);
+//     }
+// }
+// vector<vector<int>> adj; // adjacency list representation
+// int n;                   // number of nodes
+// int s;                   // source vertex
+
+// queue<int> q;
+// vector<bool> used(n);
+// vector<int> d(n), p(n);
+
+// q.push(s);
+// used[s] = true;
+// p[s] = -1;
+// while (!q.empty())
+// {
+//     int v = q.front();
+//     q.pop();
+//     for (int u : adj[v])
+//     {
+//         if (!used[u])
+//         {
+//             used[u] = true;
+//             q.push(u);
+//             d[u] = d[v] + 1;
+//             p[u] = v;
+//         }
+//     }
+// }
+
+#define loop(n) for (int i = 0; i < n; i++)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define file_read                     \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl "\n"
+#define iamspeed                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+int main(void)
+{
+    iamspeed;
+    ll n, k;
+    cin >> n >> k;
+    vi a;
+    for (int i = 0; i < k; i++)
+    {
+        ll n1 = n;
+        while (n1 > 0)
+        {
+            a.pb(n1 % 10);
+            n1 /= 10;
+        }
+        sort(a.rbegin(), a.rend());
+        n = 0;
+        n1=a.size();
+        for (int i = 0; i < n1; i++)
+        {
+            n += ll((a[i] - a[n1 - i - 1]) * pow(10, n1 - i - 1));
+        }
+        a.clear();
+    }
+    cout<<n<<endl;
+
+    return 0;
+}
